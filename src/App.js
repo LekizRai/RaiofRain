015849ae106cf33d602f018544daa6d0798df2f0
LekizRai <nguyenvuhoangtruong@gmail.com>
2024-01-02 09:39:20 +0700

    feat: first deploy raiofrain

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './Navigation';
+
+function App() {
+  return (
+    <Router>
+      <Navigation/>
+      <Routes>
+        <Route path="/home" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+      </Routes>
+  </Router>
+  );
+}
+  
+function Home() {
+  return (
+    <h1>Hello world</h1>
+    );
+}
+
+function About() {
+  return <h1>About</h1>;
+}
+
+function Contact() {
+  return <h1>Contact</h1>;
+}
+
+export default App;
