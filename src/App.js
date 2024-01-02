1c0763ecc407b5756eae0242c61243c60450c549
LekizRai <nguyenvuhoangtruong@gmail.com>
2024-01-02 23:32:55 +0700

    feat: add subpages and do homepage story entry

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ function App() {
       <Navigation/>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/story" element={<Stories/>} />
+        <Route path="/blog" element={<Blogs/>} />
+        <Route path="/project" element={<Projects/>} />
         <Route path="/about" element={<About/>} />
-        <Route path="/contact" element={<Contact/>} />
       </Routes>
     </Router>
   );
@@ -18,23 +20,61 @@ function App() {
 function Home() {
   return (
     <div className="home">
+      <div id="main">
+        <h1>Welcome to my portfolio</h1>
+        <h3>My name is Rai, not the usual way my friends call me, but just call me Rai</h3>
+      </div>
+      <div id="story">
+        <div id="story1">
+          <p style={{fontSize: '60px', fontWeight: '1000', margin: '0'}}>Let's first go through my stories</p>
+          <p style={{fontSize: '20px'}}>and discover what have made up myself so far.</p>
+        </div>
+        <div id="story2">
+          <p>Do something</p>
+        </div>
+      </div>
+      <div id="blog">
+        Or if you want to learn something not so new
+      </div>
+      <div id="project">
+        Want to get inspiration ? Come to my projects
+      </div>
+      <div id="about">
+        More information about me
+      </div>
       <h1>This is homepage</h1>
     </div>
-    );
+  );
+}
+
+function Stories() {
+  return (
+    <div className="story">
+      <h1>This is page for stories</h1>
+    </div>
+  );
+}
+
+function Blogs() {
+  return (
+    <div className="blog">
+      <h1>This is page for blogs</h1>
+    </div>
+  );
+}
+
+function Projects() {
+  return (
+    <div className="project">
+      <h1>This is page for projects</h1>
+    </div>
+  );
 }
 
 function About() {
   return (
     <div className="about">
-      <h1>This is about page</h1>
-    </div>
-    );
-}
-
-function Contact() {
-  return (
-    <div className="contact">
-      <h1>This is contact page</h1>
+      <h1>This is page about me</h1>
     </div>
     );
 }
--- src/Navigation.js
+++ src/Navigation.js
@@ -5,14 +5,17 @@ function Navigation() {
     return (
         <div className="nav">
             <div id="pageinfo">
-                <img id="pagelogo" src={process.env.PUBLIC_URL + "/logo564.png"} alt=""/>
+                <img id="pagelogo" src={process.env.PUBLIC_URL + "/logo271.png"} alt=""/>
                 <h1 id="pagename">RaiofRain</h1>
-                <h3 id="pagequote">Something about life <br/> let me tell you.</h3>
+                <h3 id="pagequote">Nothing is impossible, 
+                <br/> until it becomes impossible.</h3>
             </div>
             <nav id="navbar">
                 <Link to="/">Home</Link>
+                <Link to="/story">Stories</Link>
+                <Link to="/blog">Blogs</Link>
+                <Link to="/project">Projects</Link>
                 <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
             </nav>
         </div>
     );
